806d695c71d462275e7c0b139fd14cb72eb10193
bapan25 <ruptam25@outlook.com>
2019-06-30 19:43:38 +0530

    Extends AbstractSecurityWebApplicationInitializer to avoid SpringSecurityFilterChain

---
+++ Spring-Security/src/main/java/org/bapan/spring/security/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package org.bapan.spring.security.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * @author Ruptam
+ *
+ */
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+	
+	public SecurityWebApplicationInitializer() {
+		System.out.println("SecurityWebApplicationInitializer.SecurityWebApplicationInitializer()********");
+	}
+}
