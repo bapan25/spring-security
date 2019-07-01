d66ee54a619b2aaa9a2c096a18b90d94032ea63f
bapan25 <ruptam25@outlook.com>
2019-07-01 22:30:15 +0530

    Logout feature enabled

--- Spring-Security/src/main/java/org/bapan/spring/security/config/SecurityConfig.java
+++ Spring-Security/src/main/java/org/bapan/spring/security/config/SecurityConfig.java
@@ -44,6 +44,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.formLogin()
 				.loginPage("/showMyLoginPage")
 				.loginProcessingUrl("/authenticateTheUser")
+				.permitAll().
+				and()
+				.logout()
 				.permitAll();
 	}
 }
